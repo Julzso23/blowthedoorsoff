8fa483706bede7cb893055bd8aa30bc23ec078ca
Julxzs <julzso23@gmail.com>
2015-04-19 19:38:13 +0100

    Added Player class

---
+++ modules/player.lua
@@ -0,0 +1,31 @@
+local class = require('lib/middleclass')
+require('modules/collision')
+
+Player = class('Player')
+
+function Player:initialize()
+	self.shape = collision.collider:addRectangle(0, 0, 64, 64)
+end
+
+function Player:move(x, y)
+	self.shape:move(x, y)
+end
+function Player:setPosition(x, y)
+	self.shape:moveTo(x, y)
+end
+function Player:getPosition()
+	local x, y = self.shape:center()
+	return {x = x, y = y}
+end
+
+function Player:rotate(angle)
+	self.shape:rotate(angle)
+end
+function Player:setRotation(angle)
+	self.shape:setRotation(angle)
+end
+
+function Player:draw()
+	love.graphics.setColor(255, 255, 255, 255)
+	self.shape:draw('fill')
+end
