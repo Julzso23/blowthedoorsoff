7b45b8123deaa9f57554fec29632f693fc8365af
Julxzs <julzso23@gmail.com>
2015-04-19 20:14:37 +0100

    Implemented testing for new code

--- main.lua
+++ main.lua
@@ -1,10 +1,12 @@
 require('modules/collision')
 require('modules/player')
+require('modules/bullet')
 
 function love.load(args)
 	player = Player:new()
 	walls = {}
 	table.insert(walls, collision.collider:addRectangle(256, 256, 512, 16))
+	bullets = {}
 end
 
 function love.update(dt)
@@ -23,13 +25,31 @@ function love.update(dt)
 
 	player:setRotation(math.atan((love.mouse.getY() - player:getPosition().y) / (love.mouse.getX() - player:getPosition().x)))
 
+	for k, v in pairs(bullets) do
+		v:update(dt)
+		if v.destroyed then
+			table.remove(bullets, k)
+		end
+	end
+
 	collision.collider:update(dt)
 end
 
 function love.draw()
 	player:draw()
+
 	love.graphics.setColor(255, 0, 0, 255)
 	for k, v in pairs(walls) do
 		v:draw('fill')
+	end
+
+	for k, v in pairs(bullets) do
+		v:draw()
+	end
+end
+
+function love.mousepressed(x, y, button)
+	if button == 'l' then
+		table.insert(bullets, Bullet:new(player:getPosition().x, player:getPosition().y, player:getRotation()))
 	end
 end
